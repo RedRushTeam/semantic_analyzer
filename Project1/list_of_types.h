0776218739dfb9acd44b59c88c1d242801344c0b
aylhttd <aylhttd@gmail.com>
2019-11-12 08:57:34 +0300

    divider funnction

--- Project1/list_of_types.h
+++ Project1/list_of_types.h
@@ -22,3 +22,10 @@ using my_double = double;
 
 #define GAP 3
 #define STOP_WORD 0 
+
+my_double divider(int size) {
+	int starter = 1;
+	int finisher = size;
+	auto sum = (starter + finisher) * (finisher - starter + 1) / 2;
+	return sum;
+}
