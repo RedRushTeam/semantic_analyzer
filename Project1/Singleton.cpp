7f2310f38854de371976c12b7e7d2f55f7a42fe1
RedRushTeam <Istribion@yandex.ru>
2019-11-12 15:07:22 +0300

    UPD: asymmetry&excess ratio

--- Project1/Singleton.cpp
+++ Project1/Singleton.cpp
@@ -16,11 +16,37 @@ list<container_class> Singleton::get_list_of_container_class() const
 	return this->list_of_container_class;
 }
 
+int Singleton::get_length_of_all_container_class() const
+{
+	int counter_of_all_elems = 0;
+	for (auto obj_of_cont_class : this->list_of_container_class)
+		for (auto first_index = 0; first_index < obj_of_cont_class.get_counter_of_tokenizer(); ++first_index)
+			for (auto second_index = 0; second_index < obj_of_cont_class.get_counter_of_tokenizer(); ++second_index)
+				for (auto third_index = -GAP - 1; third_index <= GAP; ++third_index)
+					if (first_index <= second_index)
+						++counter_of_all_elems;
+	return counter_of_all_elems;
+}
+
 my_double Singleton::divider(int size) {
 	int starter = 1;
 	int finisher = size;
 	auto sum = (starter + finisher) * (finisher - starter + 1) / 2;
 	return sum;
+}
+
+my_double Singleton::calculate_sample_mean()
+{
+	my_double summ_of_all_collocations = 0;
+	for (auto obj_of_cont_class : this->list_of_container_class)
+		for (auto first_index = 0; first_index < obj_of_cont_class.get_counter_of_tokenizer(); ++first_index)
+			for (auto second_index = 0; second_index < obj_of_cont_class.get_counter_of_tokenizer(); ++second_index)
+				for (auto third_index = -GAP - 1; third_index <= GAP; ++third_index)
+					if (first_index <= second_index)
+						summ_of_all_collocations = summ_of_all_collocations + obj_of_cont_class[first_index][second_index][third_index];
+
+	my_double sample_mean = summ_of_all_collocations / this->get_length_of_all_container_class();
+	return sample_mean;
 }
 
 void Singleton::calculate_mat_ozidanie()
@@ -55,7 +81,6 @@ void Singleton::calculate_mat_disperse()
 	my_double mat_ozidanie_square = summ_of_all_collocations_2 / (this->divider(counter_of_tokenizer) * (2 + 2 * GAP));
 
 	this->mat_disperse = mat_ozidanie_square - pow(this->mat_ozidanie, 2);
-	int n = 0;
 }
 
 void Singleton::calculate_sredne_kv_otklonenie()
@@ -65,17 +90,39 @@ void Singleton::calculate_sredne_kv_otklonenie()
 
 void Singleton::calculate_sredne_kv_otklonenie_fixed()
 {
-	int counter_of_all_elems = 0;
-	for (auto obj_of_cont_class : this->list_of_container_class)
-		for (auto first_index = 0; first_index < obj_of_cont_class.get_counter_of_tokenizer(); ++first_index)
-			for (auto second_index = 0; second_index < obj_of_cont_class.get_counter_of_tokenizer(); ++second_index)
-				for (auto third_index = -GAP - 1; third_index <= GAP; ++third_index)
-					if (first_index <= second_index)
-						++counter_of_all_elems;
+	int counter_of_all_elems = this->get_length_of_all_container_class();
 
 	my_double dispersion_fixed_2 = (counter_of_all_elems / (counter_of_all_elems - 1)) * this->mat_disperse;
 
 	this->sredne_kv_otklonenie_fixed = sqrt(dispersion_fixed_2);
+}
+
+void Singleton::calculate_asymmetry_coefficient()
+{
+	my_double summ_of_m3 = 0;
+	for (auto obj_of_cont_class : this->list_of_container_class)
+		for (auto first_index = 0; first_index < obj_of_cont_class.get_counter_of_tokenizer(); ++first_index)
+			for (auto second_index = 0; second_index < obj_of_cont_class.get_counter_of_tokenizer(); ++second_index)
+				for (auto third_index = -GAP - 1; third_index <= GAP; ++third_index)
+					if (first_index <= second_index) {
+						summ_of_m3 = summ_of_m3 + pow((obj_of_cont_class[first_index][second_index][third_index] - this->calculate_sample_mean()), 3);
+					}
+
+	this->asymmetry_coefficient= (summ_of_m3 / this->get_length_of_all_container_class())/pow(this->get_sredne_kv_otklonenie_fixed(), 3);				/////zdes' ispolzovano ispravlennoe otklonenie!!!!!
+}
+
+void Singleton::calculate_excess_ratio()
+{
+	my_double summ_of_m4 = 0;
+	for (auto obj_of_cont_class : this->list_of_container_class)
+		for (auto first_index = 0; first_index < obj_of_cont_class.get_counter_of_tokenizer(); ++first_index)
+			for (auto second_index = 0; second_index < obj_of_cont_class.get_counter_of_tokenizer(); ++second_index)
+				for (auto third_index = -GAP - 1; third_index <= GAP; ++third_index)
+					if (first_index <= second_index) {
+						summ_of_m4 = summ_of_m4 + pow((obj_of_cont_class[first_index][second_index][third_index] - this->calculate_sample_mean()), 4);
+					}
+
+	this->excess_ratio = (summ_of_m4 / this->get_length_of_all_container_class()) / pow(this->get_sredne_kv_otklonenie_fixed(), 4) - 3;				/////zdes' ispolzovano ispravlennoe otklonenie!!!!!
 }
 
 my_double Singleton::get_sredne_kv_otklonenie() const
@@ -96,6 +143,16 @@ my_double Singleton::get_mat_ozidanie() const
 my_double Singleton::get_mat_disperse() const
 {
 	return this->mat_disperse;
+}
+
+my_double Singleton::get_asymmetry_coefficient() const
+{
+	return this->asymmetry_coefficient;
+}
+
+my_double Singleton::get_excess_ratio() const
+{
+	return this->excess_ratio;
 }
 
 /*bool Singleton::remove_container_class(container_class _container_class)
