c4d736f1f8509f1bc8918784e74d5cce46c9ca62
Istrebion <Istrebion@yandex.ru>
2020-02-17 14:22:02 +0300

    ГОВНОООООО

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,5 +1,5 @@
 #define LEMADR "C:\\RGD\\RussianGrammaticalDictionary\\bin-windows64\\lemmatizer.db"
-#define TEXTS_PATH "I:\\rasp_puhl"
+#define TEXTS_PATH "E:\\Новая папка (2)"
 #define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
 #define _CRT_SECURE_NO_WARNINGS
 
