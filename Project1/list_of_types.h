f59d29867a286c494f60651f5ea07ef884b4ac6b
Alexander <Istrebion@yandex.ru>
2020-03-23 01:33:30 +0300

    unneeded

--- Project1/list_of_types.h
+++ Project1/list_of_types.h
@@ -37,8 +37,11 @@ using my_double = float;
 #define GAP 2
 #define STOP_WORD 0 
 
-#define TRASH_DELETE_OPTION 0 //0 - base  1 - extended
-
+#define TRASH_DELETE_OPTION 1 //0 - base  1 - extended
+#define LEMADR "C:\\RGD\\RussianGrammaticalDictionary\\bin-windows64\\lemmatizer.db"
+#define TEXTS_PATH "E:\\Новая папка (2)"
+//#define TEXTS_PATH "A:\\rasp_puhl"
+//#define TEXTS_PATH "C:\\Users\\fortunati\\Desktop\\Новая папка (3)"
 
 enum type_of_purpose_of_cont_class {	mat_ozid_, mat_disperse_, mat_otkl_, mat_otkl_fixed_, excess_ratio_, asymmetry_coefficient_, sample_mean_all_	};
 enum type_of_cont_class {	hard_container_class_, sql_container_class_};
